7bcfd2423c33875540f8ffb1dd13c46a42c83820
KimDeokwon <codevillain@naver.com>
2019-05-23 00:12:15 +0900

    init project
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <page />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Page from './pages/index'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Page
   }
 }
 </script>
