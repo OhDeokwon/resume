531d91e80f2e2211cdf928bebbac8a555af09669
KimDeokwon <codevillain@naver.com>
2019-05-25 02:01:49 +0900

    수평선 컴포넌트 추가
    Signed-off-by: KimDeokwon <codevillain@naver.com>

---
+++ src/components/presentationals/atoms/HorizontalRule.vue
@@ -0,0 +1,34 @@
+<template>
+  <hr :class="weight" />
+</template>
+
+<script>
+export default {
+  name: 'HorizontalRule',
+  props: {
+    weight: {
+      type: String,
+      default: 'thin'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  hr {
+    display: block;
+    max-width: 100%;
+    max-height: 0;
+    height: 0;
+    border: solid;
+  }
+  .thin {
+    border-color: var(--gray);
+    border-width: thin;
+  }
+  .thick {
+    border-width: medium;
+    border-color: var(--gray-3);
+    /*border-width: thick;*/
+  }
+</style>
