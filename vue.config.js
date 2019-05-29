326a21f07b93c13f83f02045f42c6da293810695
KimDeokwon <codevillain@naver.com>
2019-05-29 18:11:38 +0900

    publicPath 릴리즈때 바뀌게 변경
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
         data: `@import "@/assets/scss/_variables.scss";`
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/resume/'
+    : '/'
 };
