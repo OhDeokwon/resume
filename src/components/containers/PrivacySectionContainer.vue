5aff6b77a6b0082c428eb76ba91a9ea8fde4f85c
KimDeokwon <codevillain@naver.com>
2019-05-29 20:28:04 +0900

    소셜 버튼 추가 및 개인정보 부분 data에서 편집가능하게 수정
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/components/containers/PrivacySectionContainer.vue
+++ src/components/containers/PrivacySectionContainer.vue
@@ -1,23 +1,27 @@
 <template>
   <PrivacySection
-    name="김덕원"
-    date="2019-05-23"
-    email="codevillain@naver.com"
-    phone="010.8557.8011"
-    introduce="
-    안녕하세요! 사용자의 입장에서 개발하는 개발자가 되기 위해 노력하는 김덕원입니다.
-    사용자들의 만족도가 곧 서비스의 완성도라 생각하며 서비스의 발전을 통해 뿌듯함을 느끼는 것을 즐깁니다.
-    "
+    :name="privacy.name"
+    :date="privacy.updated"
+    :email="privacy.email"
+    :phone="privacy.phone"
+    :introduce="privacy.introduce"
   />
 </template>
 
 <script>
 import PrivacySection from '../presentationals/organisms/PrivacySection'
 
+import data from '../../assets/scripts/data'
+
 export default {
   name: 'PrivacySectionContainer',
   components: {
     PrivacySection
+  },
+  data() {
+    return {
+      privacy: data.privacy
+    }
   }
 }
 </script>
---
+++ src/components/containers/SocialButtonsContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <social-buttons
+    :buttons = buttons
+  />
+</template>
+
+<script>
+import SocialButtons from '../presentationals/organisms/SocialButtons'
+
+import data from '../../assets/scripts/data'
+
+export default {
+  name: 'SocialButtonsContainer',
+  components: {
+    SocialButtons
+  },
+  data() {
+    return {
+      buttons: data.buttons
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
