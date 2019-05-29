3742a58a4639373d1fd1c63c88350ed2295d6ac6
KimDeokwon <codevillain@naver.com>
2019-05-29 18:01:10 +0900

    index 페이지 수정
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="container">
-    <privacy-section-container />
+    <div class="navigation-layout">
+      <!-- 아직 추가할까 말까 못 정했음 -->
+    </div>
+    <div class="main-content-layout">
+      <privacy-section-container />
+      <antecedents-section-container />
+    </div>
   </div>
 </template>
 
 <script>
 import PrivacySectionContainer from '../components/containers/PrivacySectionContainer'
+import AntecedentsSectionContainer from "../components/containers/AntecedentsSectionContainer"
 
 export default {
   name: 'index',
   components: {
+    AntecedentsSectionContainer,
     PrivacySectionContainer
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 16px 0 16px;
+  }
 </style>
