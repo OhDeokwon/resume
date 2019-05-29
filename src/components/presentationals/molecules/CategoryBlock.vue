c0d7aa4dc99b5e60bf5a38debb4f08c3d66964a2
KimDeokwon <codevillain@naver.com>
2019-05-29 17:55:21 +0900

    CategoryBlock 추가
    Signed-off-by: KimDeokwon <codevillain@naver.com>

---
+++ src/components/presentationals/molecules/CategoryBlock.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="layout">
+    <horizontal-rule weight="thick" />
+    <paragraph :text="title" class="title" />
+    <article class="article">
+      <detail-block
+        v-for="(block, index) in blocks"
+        :title="block.title"
+        :position="block.position"
+        :date="block.date"
+        :content="block.content"
+        :key="index"
+      />
+    </article>
+  </div>
+</template>
+
+<script>
+import HorizontalRule from '../atoms/HorizontalRule'
+import Paragraph from '../atoms/Paragraph'
+import DetailBlock from './DetailBlock'
+
+export default {
+  name: 'CategoryBlock',
+  components: {
+    HorizontalRule,
+    Paragraph,
+    DetailBlock
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    blocks: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .layout {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+
+    .title {
+      flex: 1 1 auto;
+      font-family: NotoSansKR-Medium;
+      margin: 16px 30px 8px 0;
+    }
+
+    .article {
+      margin-top: 16px;
+      flex: 1 1 70%;
+    }
+  }
+</style>
