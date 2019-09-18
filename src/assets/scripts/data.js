f684ee5133d7c03684a0b9ad47ba6f8699ff9560
KimDeokwon <codevillain@naver.com>
2019-09-18 15:01:20 +0900

    최신버전으로 변경
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -1,91 +1,84 @@
 export default {
-  privacy: {
-    name: '김덕원',
-    updated: '2019-05-29',
-    email: 'codevillain@naver.com',
-    phone: '010.8557.8011',
-    introduce: `안녕하세요! 사용자의 입장에서 개발하는 개발자가 되기 위해 노력하는 김덕원입니다.
+	privacy: {
+		name: '김덕원',
+		updated: '2019-09-18',
+		email: 'codevillain@naver.com',
+		phone: '010.8557.8011',
+		introduce: `안녕하세요! 사용자의 입장에서 개발하는 개발자가 되기 위해 노력하는 김덕원입니다.
     사용자들의 만족도가 곧 서비스의 완성도라 생각하며 서비스의 발전을 통해 뿌듯함을 느끼는 것을 즐깁니다.
     다른 개발자들과 기술에 대해 토론하는것을 좋아합니다.`
-  },
-  antecedents: [
-    {
-      title: '경력',
-      blocks: [
-        {
-          title: '알바체크',
-          position: 'iOS 개발자',
-          date: '2018.12 ~',
-          content: `자영업 사장님들을 위한 알바생 근태관리 어플입니다.
+	},
+	antecedents: [
+		{
+			title: '경력',
+			blocks: [
+				{
+					title: '알바체크',
+					position: 'iOS, FrontEnd 개발자',
+					date: '2018.12 ~',
+					content: `자영업 사장님들을 위한 알바생 근무관리 어플입니다.
           
-          개발 팀원이 적다보니 혼자서 많은 일들을 진행했습니다.
-          1. 하이브리드 iOS Application 개발
-          2. 하이브리드 앱에 사용될 Web FrontEnd 개발 및 유지보수
-          3. 백오피스 웹페이지 개발
-          4. 스케쥴러 및 배치 개발
-          5. 기존 서버 유지 보수
-          
-          웹은 Vue와 Vuetify를 이용하여 개발하였고 권한에 따라 화면과 작업이 다르기 때문에
-          이 부분을 중점적으로 고민하며 개발에 임했습니다.
-          
-          현재는 Native iOS Application 개발을 진행하고 있습니다.`
-        }
-      ]
-    },
-    {
-      title: '프로젝트 및 활동',
-      blocks: [
-        {
-          title: 'KB AppChallenge',
-          position: 'iOS 개발',
-          date: '2018.11',
-          content: `KB 금융지주에서 SOPT 회원들을 대상으로 2주동안 실시된 앱잼입니다.
+          Nuxt를 이용하여 개발하고 있고, 각 직급별로 다른 기능과 뷰의 관리에 초점을 맞춰
+          개발을 진행하고 있습니다.
+          또한 iOS Application 유지보수를 담당하고 있습니다.
+          `
+				}
+			]
+		},
+		{
+			title: '프로젝트 및 활동',
+			blocks: [
+				{
+					title: 'KB AppChallenge',
+					position: 'iOS 개발',
+					date: '2018.11',
+					content: `KB 금융지주에서 SOPT 회원들을 대상으로 2주동안 실시된 해커톤입니다.
           Youth(2030) 세대들을 위한 금융 서비스 제작을 주제로, 여행과 금융을 섞은
           TrableLuck이라는 서비스를 통해 대상을 수상했습니다.
           
           OpenCV를 활용한 영수증 인식을 구현했습니다.
           짧은 기간상 OpenCV로 영수증 인식만 구현하고 Microsoft Project Oxford를 사용해 OCR을 구현했습니다.`
-        },
-        {
-          title: 'SOPT 22nd',
-          position: 'iOS 개발 파트',
-          date: '2018.03 ~ 2018.07',
-          content: `대학생 연합 IT벤처 창업 동아리에 가입하여 iOS 개발 파트로서 활동했습니다.`
-        },
-        {
-          title: 'Software Maestro 8th',
-          position: '팀장',
-          date: '2017.08 ~ 2017.12',
-          content: `소프트웨어 마에스트로 8기 수료생입니다.
+				},
+				{
+					title: 'SOPT 22nd',
+					position: 'iOS 개발 파트',
+					date: '2018.03 ~ 2018.07',
+					content: `대학생 연합 IT벤처 창업 동아리에 가입하여 iOS 개발 파트로서 활동했습니다.`
+				},
+				{
+					title: 'Software Maestro 8th',
+					position: '팀장',
+					date: '2017.08 ~ 2017.12',
+					content: `소프트웨어 마에스트로 8기 수료생입니다.
           고등학교 3학년 재학중 진행했던 것으로, 컴포넌트기반 자체 엔진 개발 프로젝트를 진행했습니다.`
-        },
-        {
-          title: '우리동네 냥아치',
-          position: '팀장',
-          date: '2017.08 ~ 2017.12',
-          content: `인디 게임 카페에서 팀을 모아 제작한 게임입니다.
+				},
+				{
+					title: '우리동네 냥아치',
+					position: '팀장',
+					date: '2017.08 ~ 2017.12',
+					content: `인디 게임 카페에서 팀을 모아 제작한 게임입니다.
           간단한 게임이였지만, 원격으로 작업하는 것과, 모르는 사람들이 모여서 개발한다는 점에서 많은 어려움을 겪었습니다.
           
           현재는 정책 위반으로 잠시 내려놓은 상태이며
           약 2달만에 구글 플레이스토어 5000명 다운로드, 아케이드 게임 순위 TOP10에 진입했었습니다.`
-        },
-        {
-          title: '2984 Little Brother',
-          position: '서브 프로그래머',
-          date: '2017.11 ~ 2018.02',
-          content: `GameJam: Art, Politics and Digital Games에서 수상한 작품으로
+				},
+				{
+					title: '2984 Little Brother',
+					position: '서브 프로그래머',
+					date: '2017.11 ~ 2018.02',
+					content: `GameJam: Art, Politics and Digital Games에서 수상한 작품으로
           주한 독일 문화원의 도움을 받아 독일 A-MAZE 게임쇼에 게임을 전시했습니다.
           
           투표 및 유저의 선택에 따라 변화하는 채널 시스템 구현을 담당했습니다.
           `
-        }
-      ]
-    }
-  ],
-  buttons: [
-    {
-      image: 'images/github.png',
-      url: 'https://github.com/OhDeokwon'
-    }
-  ]
+				}
+			]
+		}
+	],
+	buttons: [
+		{
+			image: 'images/github.png',
+			url: 'https://github.com/OhDeokwon'
+		}
+	]
 }
