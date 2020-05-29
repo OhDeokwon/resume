30f0271f0cc898c1e7618e56b87c3c247ffcf83b
KimDeokwon <codevillain@naver.com>
2020-05-29 16:15:09 +0900

    깃허브 프로필 링크 변경
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -1,119 +1,124 @@
 export default {
-	privacy: {
-		name: '김덕원',
-		job: '웹 프론트엔드 개발자',
-		profile: 'https://avatars3.githubusercontent.com/u/12786855?s=460&u=f550803f0e4cfa0ff46a50b477cad324dbb1958f&v=4',
-		updated: '2020-01-20',
-		email: 'villain406@kakao.com',
-		phone: '010-8557-8011',
-		introduce: `저는 서비스의 발전 및 개선을 통해 뿌듯함을 느끼는 것을 좋아합니다. 개발자는 무조건 깔끔한 코드보다는, 필요한 코드를 작성해야한다고 생각합니다.`,
-		buttons: [
-			{
-				icon: 'images/github.svg',
-				url: 'https://github.com/OhDeokwon'
-			}
-		]
-	},
-	careers: [
-		{
-			company: '알바체크',
-			position: 'Frontend Developer',
-			date: '2018.12 ~',
-			projects: [
-				{
-					name: '알바체크 백오피스',
-					date: '2019.08 ~',
-					description: '회사 내부적으로 데이터확인 CS관리등을 위해 제작한 어드민 페이지입니다.',
-					works: [ '백오피스 전반 개발' ],
-					skills: [
-						{
-							icon: 'images/vue.svg',
-							name: 'Vue'
-						},
-						{
-							icon: 'images/typescript.svg',
-							name: 'TypeScript'
-						}
-					]
-				},
-				{
-					name: '알바체크 프론트',
-					date: '2019.01 ~',
-					description: `자영업 사장님들을 위한 매장관리 서비스입니다.
+  privacy: {
+    name: '김덕원',
+    job: '웹 프론트엔드 개발자',
+    profile: 'https://avatars3.githubusercontent.com/u/12786855?s=460&u=f550803f0e4cfa0ff46a50b477cad324dbb1958f&v=4',
+    updated: '2020-01-20',
+    email: 'villain406@kakao.com',
+    phone: '010-8557-8011',
+    introduce: `저는 서비스의 발전 및 개선을 통해 뿌듯함을 느끼는 것을 좋아합니다. 개발자는 무조건 깔끔한 코드보다는, 필요한 코드를 작성해야한다고 생각합니다.`,
+    buttons: [
+      {
+        icon: 'images/github.svg',
+        url: 'https://github.com/BangChunDeok'
+      }
+    ]
+  },
+  careers: [
+    {
+      company: '알바체크',
+      position: 'Frontend Developer',
+      date: '2018.12 ~',
+      projects: [
+        {
+          name: '알바체크 백오피스',
+          date: '2019.08 ~',
+          description: '회사 내부적으로 데이터확인 CS관리등을 위해 제작한 어드민 페이지입니다.',
+          works: [ '백오피스 전반 개발' ],
+          skills: [
+            {
+              icon: 'images/vue.svg',
+              name: 'Vue'
+            },
+            {
+              icon: 'images/typescript.svg',
+              name: 'TypeScript'
+            }
+          ]
+        },
+        {
+          name: '알바체크 프론트',
+          date: '2019.01 ~',
+          description: `자영업 사장님들을 위한 매장관리 서비스입니다.
           서비스 전체를 유지보수 및 개발하고 있습니다.
 					`,
-					works: [ '프론트 서비스 전반 개발', 'A/B 테스트 구현을 통한 사용성 개선' ],
-					skills: [
-						{
-							icon: 'images/nuxt.svg',
-							name: 'Nuxt'
-						},
-						{
-							icon: 'images/vue.svg',
-							name: 'VueX'
-						},
-						{
-							icon: 'images/javascript.svg',
-							name: 'JavaScript'
-						}
-					]
-				},
-				{
-					name: '알바체크 iOS Application 유지보수',
-					date: '2018.12 ~',
-					description: `알바체크 프론트를 웹뷰로 띄워주고 프론트에서 하기 힘든 작업들을 JS Bridge로 연결시켜 작업하고 있습니다.
+          works: [ '프론트 서비스 전반 개발', 'A/B 테스트 구현을 통한 사용성 개선' ],
+          skills: [
+            {
+              icon: 'images/nuxt.svg',
+              name: 'Nuxt'
+            },
+            {
+              icon: 'images/vue.svg',
+              name: 'VueX'
+            },
+            {
+              icon: 'images/javascript.svg',
+              name: 'JavaScript'
+            }
+          ]
+        },
+        {
+          name: '알바체크 iOS Application 유지보수',
+          date: '2018.12 ~',
+          description: `알바체크 프론트를 웹뷰로 띄워주고 프론트에서 하기 힘든 작업들을 JS Bridge로 연결시켜 작업하고 있습니다.
 					iOS Application 전체를 유지보수 및 개발하고 있습니다.`,
-					works: [ '카카오톡 초대 기능 개발', '문자 초대 기능 개발', 'JS Brdige를 인터페이스로 구조화' ],
-					skills: [
-						{
-							icon: 'images/swift.svg',
-							name: 'Swift'
-						},
-						{
-							icon: 'images/alamofire.png',
-							name: 'Alamofire'
-						}
-					]
-				}
-			]
-		}
-	],
-	activities: [
-		{
-			title: 'KB AppChallenge',
-			position: 'iOS Developer',
-			date: '2018.11',
-			description: 'KB 금융지주에서 SOPT 회원들을 대상으로 2주 동안 진행된 해커 톤입니다. OpenCV와 Project Oxford를 활용해 영수증 인식기능을 구현했습니다. 간단하게나마 앱 내에서 영상처리 기술을 시도해본 좋은 경험이었던 것 같습니다.'
-		},
-		{
-			title: 'Boost Course Ace 2nd - iOS 프로그래밍',
-			position: 'Beta Tester',
-			date: '2018.03 ~ 2018.08',
-			description: 'iOS 실무자들에게 코드리뷰를 받으며 프로젝트를 진행하는 부스트 코스의 베타테스터로 활동했었습니다. 별 탈 없이 코스를 완수했으며 코드리뷰를 통해 코드의 구조, 확장성에 대해 고민을 하며 한 단계 더 성장할 수 있었던 기회였습니다.'
-		},
-		{
-			title: 'SOPT 22nd',
-			position: 'iOS Part',
-			date: '2018.03 ~ 2018.07',
-			description: '대학생 연합 IT 벤처 창업 동아리에 가입하여 iOS 개발 파트원으로 활동했습니다. iOS 개발의 기초와 팀원 간의 협업을 배울 수 있었습니다.'
-		},
-		{
-			title: 'Software Maestro 8th',
-			position: 'Team Leader',
-			date: '2018.11',
-			description: '소프트웨어 마에스트로 8기를 수료했습니다. DX, DXUT를 활용해 컴포넌트 게임엔진을 제작했습니다. 디자인 패턴과 코드로 작성한 것들이 어떻게 화면에 그려지는지에 대해 알 수 있는 뜻깊은 프로젝트였습니다.'
-		},
-		{
-			title: '2984 Little Brother',
-			position: 'Programmer',
-			date: '2017.11 ~ 2018.02',
-			description: 'GameJam: Art, Politics and Digital Games에서 수상한 작품입니다. 주한 독일 문화원의 도움으로 독일 A-MAZE / International Playful Media Festival에 전시도 하였습니다. "내가 만든 소프트웨어에 의미를 담음으로써 얼마나 큰 영향력을 줄 수 있을까?"를 생각할 수 있었던 좋은 기회였습니다.'
-		},
-		{
-			title: '우리동네 냥아치',
-			position: 'Team Leader',
-			date: '2017.08 ~ 2017.12',
-			description: '인디 게임 카페에서 팀을 모아 제작한 게임입니다. 간단한 게임이었지만, 원격에서 작업하는 것과 모르는 사람들이 모여서 개발한다는 점에서 많은 어려움을 겪었습니다. 현재는 정책 위반으로 잠시 내려놓은 상태이며 약 1달 만에 구글 플레이스토어 5,000명 다운로드, 아케이드 게임 순위 TOP10에 진입했었습니다. 프로젝트의 완료가 얼마나 힘든 것인지 몸소 깨닫게 해준 정말 감사한 프로젝트였습니다.'
-		}
-	]
+          works: [ '카카오톡 초대 기능 개발', '문자 초대 기능 개발', 'JS Brdige를 인터페이스로 구조화' ],
+          skills: [
+            {
+              icon: 'images/swift.svg',
+              name: 'Swift'
+            },
+            {
+              icon: 'images/alamofire.png',
+              name: 'Alamofire'
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  activities: [
+    {
+      title: 'KB AppChallenge',
+      position: 'iOS Developer',
+      date: '2018.11',
+      description:
+        'KB 금융지주에서 SOPT 회원들을 대상으로 2주 동안 진행된 해커 톤입니다. OpenCV와 Project Oxford를 활용해 영수증 인식기능을 구현했습니다. 간단하게나마 앱 내에서 영상처리 기술을 시도해본 좋은 경험이었던 것 같습니다.'
+    },
+    {
+      title: 'Boost Course Ace 2nd - iOS 프로그래밍',
+      position: 'Beta Tester',
+      date: '2018.03 ~ 2018.08',
+      description:
+        'iOS 실무자들에게 코드리뷰를 받으며 프로젝트를 진행하는 부스트 코스의 베타테스터로 활동했었습니다. 별 탈 없이 코스를 완수했으며 코드리뷰를 통해 코드의 구조, 확장성에 대해 고민을 하며 한 단계 더 성장할 수 있었던 기회였습니다.'
+    },
+    {
+      title: 'SOPT 22nd',
+      position: 'iOS Part',
+      date: '2018.03 ~ 2018.07',
+      description: '대학생 연합 IT 벤처 창업 동아리에 가입하여 iOS 개발 파트원으로 활동했습니다. iOS 개발의 기초와 팀원 간의 협업을 배울 수 있었습니다.'
+    },
+    {
+      title: 'Software Maestro 8th',
+      position: 'Team Leader',
+      date: '2018.11',
+      description:
+        '소프트웨어 마에스트로 8기를 수료했습니다. DX, DXUT를 활용해 컴포넌트 게임엔진을 제작했습니다. 디자인 패턴과 코드로 작성한 것들이 어떻게 화면에 그려지는지에 대해 알 수 있는 뜻깊은 프로젝트였습니다.'
+    },
+    {
+      title: '2984 Little Brother',
+      position: 'Programmer',
+      date: '2017.11 ~ 2018.02',
+      description:
+        'GameJam: Art, Politics and Digital Games에서 수상한 작품입니다. 주한 독일 문화원의 도움으로 독일 A-MAZE / International Playful Media Festival에 전시도 하였습니다. "내가 만든 소프트웨어에 의미를 담음으로써 얼마나 큰 영향력을 줄 수 있을까?"를 생각할 수 있었던 좋은 기회였습니다.'
+    },
+    {
+      title: '우리동네 냥아치',
+      position: 'Team Leader',
+      date: '2017.08 ~ 2017.12',
+      description:
+        '인디 게임 카페에서 팀을 모아 제작한 게임입니다. 간단한 게임이었지만, 원격에서 작업하는 것과 모르는 사람들이 모여서 개발한다는 점에서 많은 어려움을 겪었습니다. 현재는 정책 위반으로 잠시 내려놓은 상태이며 약 1달 만에 구글 플레이스토어 5,000명 다운로드, 아케이드 게임 순위 TOP10에 진입했었습니다. 프로젝트의 완료가 얼마나 힘든 것인지 몸소 깨닫게 해준 정말 감사한 프로젝트였습니다.'
+    }
+  ]
 }
