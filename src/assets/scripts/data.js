d4d8422bafcbb4fe32b9facad0b65240b480805a
Kim Deokwon <codevillain@naver.com>
2020-06-18 00:27:09 +0900

    공모전 수상항목 추가
    Signed-off-by: Kim Deokwon <codevillain@naver.com>

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -80,16 +80,25 @@ export default {
 	],
 	activities: [
 		{
-			title: 'KB AppChallenge',
+			title: '건축정보 모바일서비스 콘텐츠 개발 공모전 최우수상',
+			position: 'iOS Developer',
+			date: '2019.11 ~ 2020.02',
+			description:
+				'국토교통부와 한국감정원이 공동개최한 공모전으로 AR을 활용하여 주변 건물에서 교통약자를 위한 시설이 있는지 확인 할 수 있는 앱을 제작했습니다. barrier free 제도에 대해 알게 되었고 AR을 시도하여 서비스를 제작했다는 것에 큰 의의를 두고 있습니다.'
+		},
+		{
+			title: 'KB AppChallenge 대상',
 			position: 'iOS Developer',
 			date: '2018.11',
-			description: 'KB 금융지주에서 SOPT 회원들을 대상으로 2주 동안 진행된 해커 톤입니다. OpenCV와 Project Oxford를 활용해 영수증 인식기능을 구현했습니다. 간단하게나마 앱 내에서 영상처리 기술을 시도해본 좋은 경험이었던 것 같습니다.'
+			description:
+				'KB 금융지주에서 SOPT 회원들을 대상으로 2주 동안 진행된 해커 톤입니다. OpenCV와 Project Oxford를 활용해 영수증 인식기능을 구현했습니다. 간단하게나마 앱 내에서 영상처리 기술을 시도해본 좋은 경험이었던 것 같습니다.'
 		},
 		{
 			title: 'Boost Course Ace 2nd - iOS 프로그래밍',
 			position: 'Beta Tester',
 			date: '2018.03 ~ 2018.08',
-			description: 'iOS 실무자들에게 코드리뷰를 받으며 프로젝트를 진행하는 부스트 코스의 베타테스터로 활동했었습니다. 별 탈 없이 코스를 완수했으며 코드리뷰를 통해 코드의 구조, 확장성에 대해 고민을 하며 한 단계 더 성장할 수 있었던 기회였습니다.'
+			description:
+				'iOS 실무자들에게 코드리뷰를 받으며 프로젝트를 진행하는 부스트 코스의 베타테스터로 활동했었습니다. 별 탈 없이 코스를 완수했으며 코드리뷰를 통해 코드의 구조, 확장성에 대해 고민을 하며 한 단계 더 성장할 수 있었던 기회였습니다.'
 		},
 		{
 			title: 'SOPT 22nd',
@@ -101,19 +110,22 @@ export default {
 			title: 'Software Maestro 8th',
 			position: 'Team Leader',
 			date: '2018.11',
-			description: '소프트웨어 마에스트로 8기를 수료했습니다. DX, DXUT를 활용해 컴포넌트 게임엔진을 제작했습니다. 디자인 패턴과 코드로 작성한 것들이 어떻게 화면에 그려지는지에 대해 알 수 있는 뜻깊은 프로젝트였습니다.'
+			description:
+				'소프트웨어 마에스트로 8기를 수료했습니다. DX, DXUT를 활용해 컴포넌트 게임엔진을 제작했습니다. 디자인 패턴과 코드로 작성한 것들이 어떻게 화면에 그려지는지에 대해 알 수 있는 뜻깊은 프로젝트였습니다.'
 		},
 		{
 			title: '2984 Little Brother',
 			position: 'Programmer',
 			date: '2017.11 ~ 2018.02',
-			description: 'GameJam: Art, Politics and Digital Games에서 수상한 작품입니다. 주한 독일 문화원의 도움으로 독일 A-MAZE / International Playful Media Festival에 전시도 하였습니다. "내가 만든 소프트웨어에 의미를 담음으로써 얼마나 큰 영향력을 줄 수 있을까?"를 생각할 수 있었던 좋은 기회였습니다.'
+			description:
+				'GameJam: Art, Politics and Digital Games에서 수상한 작품입니다. 주한 독일 문화원의 도움으로 독일 A-MAZE / International Playful Media Festival에 전시도 하였습니다. "내가 만든 소프트웨어에 의미를 담음으로써 얼마나 큰 영향력을 줄 수 있을까?"를 생각할 수 있었던 좋은 기회였습니다.'
 		},
 		{
 			title: '우리동네 냥아치',
 			position: 'Team Leader',
 			date: '2017.08 ~ 2017.12',
-			description: '인디 게임 카페에서 팀을 모아 제작한 게임입니다. 간단한 게임이었지만, 원격에서 작업하는 것과 모르는 사람들이 모여서 개발한다는 점에서 많은 어려움을 겪었습니다. 현재는 정책 위반으로 잠시 내려놓은 상태이며 약 1달 만에 구글 플레이스토어 5,000명 다운로드, 아케이드 게임 순위 TOP10에 진입했었습니다. 프로젝트의 완료가 얼마나 힘든 것인지 몸소 깨닫게 해준 정말 감사한 프로젝트였습니다.'
+			description:
+				'인디 게임 카페에서 팀을 모아 제작한 게임입니다. 간단한 게임이었지만, 원격에서 작업하는 것과 모르는 사람들이 모여서 개발한다는 점에서 많은 어려움을 겪었습니다. 현재는 정책 위반으로 잠시 내려놓은 상태이며 약 1달 만에 구글 플레이스토어 5,000명 다운로드, 아케이드 게임 순위 TOP10에 진입했었습니다. 프로젝트의 완료가 얼마나 힘든 것인지 몸소 깨닫게 해준 정말 감사한 프로젝트였습니다.'
 		}
 	]
 }
